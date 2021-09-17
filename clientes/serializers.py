ec54ce3381a9e0af6ba945cafc77f044fdf14eb6
Jovennan Ramalho <jovennan@gmail.com>
2021-09-17 10:51:39 -0300

    validate CPF

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -8,7 +8,7 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({'cpf': "O CPF inválido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': "O Nome deve ter apenas caracteres alfabéticos"})
         if not rg_valido(data['rg']):
